478ae85d0a7c6445f0bd61b21347fc05380527c1
Abed Islam <aislam360@gmail.com>
2020-03-08 23:58:31 -0400

    buttons work. logic doesnt

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -3,25 +3,61 @@ import React from 'react';
 import Box from "./Box.js"
 import funkoData from "../Data.js";
 
-function Wrapper() {
+class Wrapper extends React.Component{
 
-	const funkoComponents = funkoData.map(funko => <Box key={funko.id} img={funko.img} 
-							name={funko.name} category={funko.category} value={funko.value} />)
+	constructor(){
+		super()
+		this.state = {
+			showMe: 0
+		}
+	}
 
-	return (
-		<div>
-			<navbar className="buttons">
-				<button type="button">Games</button>
-				<button type="button">Television</button>
-				<button type="button">Animation</button>
-				<button type="button">All</button>
-			</navbar>
+
+	render(){
+
+
+
+		const funkoComponentsAll = funkoData.map(funko => <Box key={funko.id} img={funko.img} 
+								name={funko.name} category={funko.category} value={funko.value} />)
+
+		const funkoComponentsGames = funkoData.map(funko => <Box key={funko.id} img={funko.img} 
+								name={funko.name} category={funko.category = "games"} value={funko.value} />)
+
+		const funkoComponentsTelevision = funkoData.map(funko => <Box key={funko.id} img={funko.img} 
+								name={funko.name} category={funko.category = "television"} value={funko.value} />)
+
+		const funkoComponentsAnimation = funkoData.map(funko => <Box key={funko.id} img={funko.img} 
+								name={funko.name} category={funko.category = "animation"} value={funko.value} />)
+
+		
+		return (
+
 			<div>
-				{funkoComponents}
+				<navbar className="buttons">
+					<button type="button" onClick={() => this.setState({showMe: 1})}>Games</button>
+					<button type="button" onClick={() => this.setState({showMe: 2})}>Television</button>
+					<button type="button" onClick={() => this.setState({showMe: 3})}>Animation</button>
+					<button type="button" onClick={() => this.setState({showMe: 0})}>All</button>
+				</navbar>
+				<div>
+					{(() => {
+				        switch (this.state.showMe) {
+				          case 0: return funkoComponentsAll;
+				          case 1: return funkoComponentsGames;
+				          case 2: return funkoComponentsTelevision;
+				          case 3: return funkoComponentsAnimation;
+				          default: return funkoComponentsAll;
+				        }
+				    })()}
+				</div>
+
+				
 			</div>
-			
-		</div>
-	);
+		);		
+
+	}
+
+
 }
 
 export default Wrapper;
